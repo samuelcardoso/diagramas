a740b9c0a44e1a65fd0a80fd5e743112baf8223e
Samuel Cardoso <samuelparacontato@gmail.com>
2025-06-22 19:53:32 -0300

    fix:cors

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://samuelcardoso.github.io/diagramas/',
+  base: 'https://samuelcardoso.github.io/diagramas/dist/',
   plugins: [react()],
   server: {
     fs: {
